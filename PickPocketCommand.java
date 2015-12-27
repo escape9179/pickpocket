3fd974d3feab77bdda57889299722240d40e58d8
Tre Logan <tr3.logan@gmail.com>
2015-12-27 01:57:08 -0500

    Test

---
+++ PickPocketCommand.java
@@ -0,0 +1,17 @@
+package logan.pickpocket.main;
+
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
+
+/**
+ * Created by Tre on 12/15/2015.
+ */
+public abstract class PickPocketCommand {
+    public boolean execute(Player player) {
+        return true;
+    }
+
+    public boolean execute(Player player, Command command, String label, Object... args) {
+        return true;
+    }
+}
