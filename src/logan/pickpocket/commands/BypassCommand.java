449003d6c4bcbdeb2e2823bd0dce4eb4ce6c1241
Tre Logan <tr3.logan@gmail.com>
2020-08-08 11:21:48 -0400

    Permission check for bypass and exempt commands

--- src/logan/pickpocket/commands/BypassCommand.java
+++ src/logan/pickpocket/commands/BypassCommand.java
@@ -1,5 +1,6 @@
 package logan.pickpocket.commands;
 
+import logan.pickpocket.main.PickpocketPlugin;
 import logan.pickpocket.main.Profiles;
 import logan.pickpocket.profile.Profile;
 import org.bukkit.Bukkit;
@@ -14,18 +15,28 @@ import java.util.List;
 public class BypassCommand implements PickpocketCommand {
 
     @Override
-    public <T> void execute(Player player, List<Profile> profiles, T... objects) {
-        
+    public <T> void execute(Player player, List<Profile> profiles, T... objects)
+    {
+
         //TODO Fix array index out of bounds here
-        
-        if (objects.length == 0) {
+
+        if (!player.hasPermission(PickpocketPlugin.PICKPOCKET_BYPASS))
+        {
+            player.sendMessage(ChatColor.RED + "No permission.");
+            return;
+        }
+
+        if (objects.length == 0)
+        {
             Profile profile = Profiles.get(player);
-            boolean bool = profile.getProfileConfiguration().getBypassSectionValue();
+            boolean bool    = profile.getProfileConfiguration().getBypassSectionValue();
             profile.getProfileConfiguration().setBypassSection(bool = !bool);
             player.sendMessage(ChatColor.GRAY + "Your bypass status has been changed to " + bool + ".");
-        } else {
-            Player otherPlayer = Bukkit.getPlayer(objects[1].toString());
-            boolean bool = Boolean.parseBoolean(objects[0].toString());
+        }
+        else
+        {
+            Player  otherPlayer        = Bukkit.getPlayer(objects[1].toString());
+            boolean bool               = Boolean.parseBoolean(objects[0].toString());
             Profile otherPlayerProfile = Profiles.get(otherPlayer);
             otherPlayerProfile.getProfileConfiguration().setBypassSection(bool);
             player.sendMessage(ChatColor.GRAY + "Changed " + otherPlayer.getName() + "'s bypass status to " + bool + ".");
--- src/logan/pickpocket/commands/ExemptCommand.java
+++ src/logan/pickpocket/commands/ExemptCommand.java
@@ -1,5 +1,6 @@
 package logan.pickpocket.commands;
 
+import logan.pickpocket.main.PickpocketPlugin;
 import logan.pickpocket.main.Profiles;
 import logan.pickpocket.profile.Profile;
 import org.bukkit.Bukkit;
@@ -13,17 +14,28 @@ import java.util.List;
  */
 public class ExemptCommand implements PickpocketCommand {
 
+
     @Override
-    public <T> void execute(Player player, List<Profile> profiles, T... objects) {
-        if (objects.length == 0) {
+    public <T> void execute(Player player, List<Profile> profiles, T... objects)
+    {
+
+        if (!player.hasPermission(PickpocketPlugin.PICKPOCKET_EXEMPT))
+        {
+            player.sendMessage(ChatColor.RED + "No permission.");
+            return;
+        }
+
+        if (objects.length == 0)
+        {
             Profile profile = Profiles.get(player);
-            boolean bool = profile.getProfileConfiguration().getExemptSectionValue();
+            boolean bool    = profile.getProfileConfiguration().getExemptSectionValue();
             profile.getProfileConfiguration().setExemptSection(bool = !bool);
             player.sendMessage(ChatColor.GRAY + "Your exempt status has been changed to " + bool + ".");
         }
-        else {
-            Player otherPlayer = Bukkit.getPlayer(objects[1].toString());
-            boolean bool = Boolean.parseBoolean(objects[0].toString());
+        else
+        {
+            Player  otherPlayer        = Bukkit.getPlayer(objects[1].toString());
+            boolean bool               = Boolean.parseBoolean(objects[0].toString());
             Profile otherPlayerProfile = Profiles.get(otherPlayer);
             otherPlayerProfile.getProfileConfiguration().setExemptSection(bool);
             player.sendMessage(ChatColor.GRAY + "Changed " + otherPlayer.getName() + "'s exempt status to " + bool + ".");
