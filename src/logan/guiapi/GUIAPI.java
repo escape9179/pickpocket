dc9c589be71fd1432a897efe2035a31622ccdaff
Tre Logan <tr3.logan@gmail.com>
2020-08-07 21:08:14 -0400

    Fix some bugs related to GUI menus
    Closes #27

--- src/logan/guiapi/GUIAPI.java
+++ src/logan/guiapi/GUIAPI.java
@@ -1,47 +1,81 @@
 package logan.guiapi;
 
 import logan.guiapi.util.PlaceholderManager;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Tre Logan
  */
-public class GUIAPI {
+public class GUIAPI
+{
 
-    private static Map<Integer, Menu> registeredMenus = new ConcurrentHashMap<>();
+    private static Map<Integer, Menu> registeredMenus    = new ConcurrentHashMap<>();
     private static PlaceholderManager placeholderManager = new PlaceholderManager();
 
-    public static void registerMenu(int id, Menu menu) {
+    public static void registerMenu(int id, Menu menu)
+    {
         if (registeredMenus.containsKey(id)) return;
         registeredMenus.put(id, menu);
     }
 
-    public static PlaceholderManager getPlaceholderManager() {
+    public static PlaceholderManager getPlaceholderManager()
+    {
         return placeholderManager;
     }
 
-    public static void callInventoryClickEvents(InventoryClickEvent event) {
-        for (int key : registeredMenus.keySet()) {
+    public static void callInventoryClickEvents(InventoryClickEvent event)
+    {
+        for (int key : registeredMenus.keySet())
+        {
             System.out.println("Calling event for menu " + key + ".");
             registeredMenus.get(key).onInventoryClick(event);
         }
     }
 
-    public static void callInventoryCloseEvents(InventoryCloseEvent event) {
-        if (registeredMenus.isEmpty()) return;
-        Iterator<Integer> menuIterator = registeredMenus.keySet().iterator();
-        while (menuIterator.hasNext()) {
-            int id = menuIterator.next();
-            Menu menu = registeredMenus.get(id);
-            if (menu.shouldClose()) {
-                menuIterator.remove();
-                registeredMenus.remove(id);
-            }
+    public static void callInventoryCloseEvents(InventoryCloseEvent event)
+    {
+        if (registeredMenus.isEmpty())
+        {
+            System.out.println("Nothing to close...");
+            return;
         }
+        Iterator<Integer> menuIterator = registeredMenus.keySet().iterator();
+        while (menuIterator.hasNext())
+        {
+            Menu        registeredMenu = registeredMenus.get(menuIterator.next());
+            Inventory   inventory      = event.getInventory();
+            HumanEntity viewer         = inventory.getViewers().get(0);
+
+            if (registeredMenu.getViewer().equals(viewer))
+            {
+                registeredMenu.setCloseCalledFromEvent(true);
+                registeredMenu.close();
+                System.out.println("Viewers are equal. Automatically closing menu.");
+                break;
+            }
+
+            System.out.println("Viewers aren't equal.");
+        }
+
+        List<Integer> menusToClose = new ArrayList<>();
+
+        registeredMenus.forEach((k, v) ->
+        {
+            if (registeredMenus.get(k).isClosed()) menusToClose.add(k);
+        });
+        menusToClose.forEach(menuId ->
+        {
+            registeredMenus.remove(menuId);
+            System.out.println("Removed menu with id: " + menuId);
+        });
     }
 }
--- src/logan/guiapi/Menu.java
+++ src/logan/guiapi/Menu.java
@@ -15,7 +15,8 @@ import java.util.stream.Stream;
 /**
  * @author Tre Logan
  */
-public class Menu {
+public class Menu
+{
 
     private static int nextId = 0;
 
@@ -23,24 +24,28 @@ public class Menu {
     private       String    title;
     private       Inventory inventory;
     private       int       slots;
-    private       boolean   shouldClose = false;
+    private       boolean   closeCalledFromEvent;
+    private       boolean   closed;
 
     private Player viewer = null;
 
     private Map<Integer, MenuItem> menuItems = new HashMap<>();
 
-    public Menu(String title, int rows) {
+    public Menu(String title, int rows)
+    {
         id         = nextId;
         this.title = title;
         slots      = rows * 9;
         nextId++;
     }
 
-    public int getId() {
+    public int getId()
+    {
         return id;
     }
 
-    public void show(Player player) {
+    public void show(Player player)
+    {
         /* Create inventory and add items */
         inventory = Bukkit.createInventory(player, slots, title);
         menuItems.forEach((s, mi) -> inventory.setItem(s, mi.getItemStack()));
@@ -51,18 +56,28 @@ public class Menu {
         player.openInventory(inventory);
     }
 
-    public boolean shouldClose() {
-        return shouldClose;
+    public void setCloseCalledFromEvent(boolean value)
+    {
+        closeCalledFromEvent = value;
     }
 
-    public void close() {
-        shouldClose = true;
-        viewer.closeInventory();
+    public void close()
+    {
+        if (!closeCalledFromEvent) viewer.closeInventory();
+        else System.out.println("Closed was called manually.");
+        closed = true;
     }
 
-    private void parsePlaceholders(Player player) {
+    public boolean isClosed()
+    {
+        return closed;
+    }
+
+    private void parsePlaceholders(Player player)
+    {
         title = PlaceholderParser.parse(title, player);
-        menuItems.forEach((s, mi) -> {
+        menuItems.forEach((s, mi) ->
+        {
             mi.setName(PlaceholderParser.parse(mi.getDisplayName(), player));
             List<String>   lore   = mi.getLore();
             Stream<String> stream = lore.stream().map(l -> PlaceholderParser.parse(l, player));
@@ -71,71 +86,92 @@ public class Menu {
         });
     }
 
-    public void setTitle(String title) {
+    public void setTitle(String title)
+    {
         this.title = title;
     }
 
-    public void setRows(int rows) {
+    public void setRows(int rows)
+    {
         this.slots = rows * 9;
     }
 
-    public void fill(Filler fillPattern) {
+    public void fill(Filler fillPattern)
+    {
         this.fill(fillPattern, Collections.emptyList(), FillPlacer.FillMode.IGNORE);
     }
 
-    public void fill(Filler fillPattern, Collection<Integer> slots, FillPlacer.FillMode mode) {
+    public void fill(Filler fillPattern, Collection<Integer> slots, FillPlacer.FillMode mode)
+    {
         fillPattern.fill(this, slots, mode);
     }
 
-    public MenuItem addItem(int slot, MenuItem menuItem) {
+    public MenuItem addItem(int slot, MenuItem menuItem)
+    {
         return menuItems.put(slot, menuItem);
     }
 
-    public void removeItem(int slot, MenuItem menuItem) {
+    public void removeItem(int slot, MenuItem menuItem)
+    {
         menuItems.remove(slot);
     }
 
-    public Map<Integer, MenuItem> getMenuItems() {
+    public Map<Integer, MenuItem> getMenuItems()
+    {
         return menuItems;
     }
 
-    public String getTitle() {
+    public String getTitle()
+    {
         return title;
     }
 
-    public Inventory getInventory() {
+    public Inventory getInventory()
+    {
         return inventory;
     }
 
-    public int getSlots() {
+    public int getSlots()
+    {
         return slots;
     }
 
-    public int getTopLeft() {
+    public int getTopLeft()
+    {
         return 0;
     }
 
-    public int getTopRight() {
+    public int getTopRight()
+    {
         return 8;
     }
 
-    public int getBottomLeft() {
+    public int getBottomLeft()
+    {
         return slots - 9;
     }
 
-    public int getBottomRight() {
+    public int getBottomRight()
+    {
         return slots - 1;
     }
 
-    public Player getViewer() {
+    public Player getViewer()
+    {
         return viewer;
     }
 
-    public void onInventoryClick(InventoryClickEvent event) {
+    public void onInventoryClick(InventoryClickEvent event)
+    {
 
-        if (!(viewer.getUniqueId()).equals(event.getWhoClicked().getUniqueId())) return;
+        if (!(viewer.getUniqueId()).equals(event.getWhoClicked().getUniqueId()))
+        {
+            System.out.println("No the right viewer.");
+            return;
+        }
 
         event.setCancelled(true);
+        System.out.println("Still viewing inventory. Event cancelled.");
 
         menuItems.keySet().stream()
                 .filter(s -> s == event.getSlot())
