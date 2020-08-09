cae2fc20e1cde9ff6b404a28d7148107721ae7ac
Tre Logan <tr3.logan@gmail.com>
2020-08-09 15:27:10 -0400

    made status messages configurable
    These are the messages sent to a player when they login, and when attempting to interact with another player while they have pick-pocketing disabled, or the other player does.

--- src/logan/pickpocket/listeners/PlayerInteract.java
+++ src/logan/pickpocket/listeners/PlayerInteract.java
@@ -23,56 +23,49 @@ import java.util.List;
 /**
  * Created by Tre on 12/28/2015.
  */
-public class PlayerInteract implements Listener
-{
+public class PlayerInteract implements Listener {
 
-    public PlayerInteract()
-    {
+    public PlayerInteract() {
         PickpocketPlugin.registerListener(this);
     }
 
     @EventHandler
-    public void onPlayerInteract(PlayerInteractEntityEvent event)
-    {
+    public void onPlayerInteract(PlayerInteractEntityEvent event) {
         PickpocketPlugin pickpocketPlugin = PickpocketPlugin.getInstance();
 
         if (!(event.getRightClicked() instanceof Player) ||
                 !event.getHand().equals(EquipmentSlot.OFF_HAND) ||
                 !event.getPlayer().isSneaking()) return;
 
-        Player  player  = event.getPlayer();
-        Player  victim  = (Player) event.getRightClicked();
+        Player player = event.getPlayer();
+        Player victim = (Player) event.getRightClicked();
         Profile profile = Profiles.get(player);
 
-        if (!Profiles.get(victim).isParticipating())
-        {
-            player.sendMessage(ChatColor.RED + "That player has pick-pocketing disabled.");
+        if (!Profiles.get(victim).isParticipating()) {
+            if (PickpocketPlugin.isShowStatusOnInteractEnabled())
+                player.sendMessage(ChatColor.RED + "That player has pick-pocketing disabled.");
             return;
         }
 
-        if (!profile.isParticipating())
-        {
-            player.sendMessage(ChatColor.RED + "You have pick-pocketing disabled.");
+        if (!profile.isParticipating()) {
+            if (PickpocketPlugin.isShowStatusOnInteractEnabled())
+                player.sendMessage(ChatColor.RED + "You have pick-pocketing disabled.");
             return;
         }
 
-        if (!pickpocketPlugin.getCooldowns().containsKey(player))
-        {
+        if (!pickpocketPlugin.getCooldowns().containsKey(player)) {
             final int numberOfRandomItems = 4;
 
             showNewRummageMenu(player, victim, getRandomItemsFromPlayer(victim, numberOfRandomItems));
 
             profile.setRummaging(true);
-        }
-        else
-        {
+        } else {
             player.sendMessage(ChatColor.RED + "You must wait " + pickpocketPlugin.getCooldowns().get(player) + " seconds before attempting another pickpocket.");
         }
     }
 
-    private void showNewRummageMenu(Player player, Player victim, List<ItemStack> randomItems)
-    {
-        Menu     rummageMenu   = new Menu("Rummage", 4);
+    private void showNewRummageMenu(Player player, Player victim, List<ItemStack> randomItems) {
+        Menu rummageMenu = new Menu("Rummage", 4);
         MenuItem rummageButton = new MenuItem("Keep rummaging...", new ItemStack(Material.CHEST));
 
         rummageMenu.fill(new UniFill(Material.WHITE_STAINED_GLASS_PANE));
@@ -84,15 +77,14 @@ public class PlayerInteract implements Listener
         });
         rummageMenu.addItem(rummageMenu.getBottomRight(), rummageButton);
 
-        for (int i = 0; i < randomItems.size(); i++)
-        {
+        for (int i = 0; i < randomItems.size(); i++) {
             MenuItem menuItem = new MenuItem(randomItems.get(i));
             menuItem.addListener(clickEvent ->
             {
-                final ItemStack fillerItem      = new ItemStack(Material.WHITE_STAINED_GLASS_PANE);
-                final Inventory inventory       = rummageMenu.getInventory();
-                final Profile   profile         = Profiles.get(player);
-                final int       bottomRightSlot = rummageMenu.getBottomRight();
+                final ItemStack fillerItem = new ItemStack(Material.WHITE_STAINED_GLASS_PANE);
+                final Inventory inventory = rummageMenu.getInventory();
+                final Profile profile = Profiles.get(player);
+                final int bottomRightSlot = rummageMenu.getBottomRight();
 
                 inventory.setItem(bottomRightSlot, fillerItem);
                 player.closeInventory();
@@ -106,8 +98,7 @@ public class PlayerInteract implements Listener
         rummageMenu.show(player);
 
         // 1/5 chance that the player will get caught while rummaging.
-        if (Math.random() < 0.1)
-        {
+        if (Math.random() < 0.1) {
             victim.sendMessage(ChatColor.RED + "You feel something touch your side.");
 
             // Close the rummage inventory
@@ -119,25 +110,22 @@ public class PlayerInteract implements Listener
         player.playSound(player.getLocation(), Sound.BLOCK_SNOW_STEP, 1.0f, 0.5f);
     }
 
-    private List<ItemStack> getRandomItemsFromPlayer(Player player, int numberOfItems)
-    {
-        final List<ItemStack> randomItemList  = new ArrayList<>();
-        final ItemStack[]     storageContents = player.getInventory().getStorageContents();
-        final int             inventorySize   = player.getInventory().getStorageContents().length;
-        ItemStack             randomItem;
-        int                   randomSlot;
+    private List<ItemStack> getRandomItemsFromPlayer(Player player, int numberOfItems) {
+        final List<ItemStack> randomItemList = new ArrayList<>();
+        final ItemStack[] storageContents = player.getInventory().getStorageContents();
+        final int inventorySize = player.getInventory().getStorageContents().length;
+        ItemStack randomItem;
+        int randomSlot;
 
         outer:
-        for (int i = 0; i < numberOfItems; i++)
-        {
+        for (int i = 0; i < numberOfItems; i++) {
             randomSlot = 9 + (int) (Math.random() * (inventorySize - 9));
             randomItem = storageContents[randomSlot];
 
             if (randomItem == null) continue;
 
             // Check if the item is banned
-            for (String disabledItem : PickpocketPlugin.getDisabledItems())
-            {
+            for (String disabledItem : PickpocketPlugin.getDisabledItems()) {
                 Material disabledItemType = Material.getMaterial(disabledItem.toUpperCase());
 
                 // This item is disabled. Skip this random item iteration.
--- src/logan/pickpocket/listeners/PlayerJoin.java
+++ src/logan/pickpocket/listeners/PlayerJoin.java
@@ -12,27 +12,25 @@ import org.bukkit.event.player.PlayerJoinEvent;
 /**
  * Created by Tre on 12/28/2015.
  */
-public class PlayerJoin implements Listener
-{
-    public PlayerJoin()
-    {
+public class PlayerJoin implements Listener {
+    public PlayerJoin() {
         PickpocketPlugin.registerListener(this);
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event)
-    {
+    public void onPlayerJoin(PlayerJoinEvent event) {
 
-        Player  player  = event.getPlayer();
+        Player player = event.getPlayer();
         Profile profile = Profiles.get(player);
 
-        if (profile.isParticipating())
-        {
+        // return without showing status message
+        if (!PickpocketPlugin.isShowStatusOnLoginEnabled()) return;
+
+        // show status message
+        if (profile.isParticipating()) {
             player.sendMessage(ChatColor.GRAY + "You are currently participating in pick-pocketing.");
             player.sendMessage(ChatColor.GRAY + "Use '/pickpocket toggle' to disable pick-pocketing.");
-        }
-        else
-        {
+        } else {
             player.sendMessage(ChatColor.GRAY + "You are currently not participating in pick-pocketing.");
             player.sendMessage(ChatColor.GRAY + "Use '/pickpocket toggle' to enable pick-pocketing.");
         }
