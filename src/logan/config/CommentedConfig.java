bf6b867ce43cad2360e27ae0c574eaffe0587ad4
Tre Logan <tr3.logan@gmail.com>
2020-08-21 22:26:53 -0400

    Revert "Added constructor to create configs from files"
    This reverts commit 2a5c4798

--- src/logan/config/CommentedConfig.java
+++ src/logan/config/CommentedConfig.java
@@ -1,113 +1,63 @@
 package logan.config;
 
-import logan.pickpocket.main.PickpocketPlugin;
 import org.bukkit.configuration.file.YamlConfiguration;
 
-import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class CommentedConfig {
+    private Map<String, String[]> commentToKeyMap = new HashMap<>();
+    private YamlConfiguration configuration;
+    private File file;
 
-    public static String COMMENT_PREFIX = "# ";
-    public static String KEY_VALUE_SEPARATOR = ":";
-    private static final String lineSeparator = System.lineSeparator();
-
-    private Map<String, String> keyCommentMap;
-
-    private YamlConfiguration configuration = new YamlConfiguration();
-    private Path filePath;
-
-    public CommentedConfig(InputStream inputStream, String destPath) {
-        filePath = Paths.get(destPath);
-        loadConfiguration();
-        StringBuilder stringBuilder = new StringBuilder();
-        int currByte;
+    /**
+     * Create a commented config from an existing file.
+     */
+    public CommentedConfig(File file) {
+        this.file = file;
         try {
-            while ((currByte = inputStream.read()) != -1) {
-                stringBuilder.append((char) currByte);
-            }
+            file.createNewFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        keyCommentMap = getKeyCommentMapFromString(stringBuilder.toString());
+        configuration = YamlConfiguration.loadConfiguration(file);
+    }
+
+    public void createKeyIfNoneExists(String key, Object defaultValue) {
+        if (!configuration.isSet(key)) {
+            configuration.set(key, defaultValue);
+        }
     }
 
     /**
-     * Creates a commented config from a string.
+     * Add a comment to a configuration key.
      */
-    public CommentedConfig(String contents, String destPath) {
-        filePath = Paths.get(destPath);
-        loadConfiguration();
-        keyCommentMap = getKeyCommentMapFromString(contents);
-    }
-
-    private Map<String, String> getKeyCommentMapFromString(String contents) {
-        Map<String, String> keyCommentMap = new HashMap<>();
-        Scanner scanner = new Scanner(contents);
-        String currentLine;
-        StringBuilder currentComment = new StringBuilder();
-        while (scanner.hasNext()) {
-            currentLine = scanner.nextLine();
-            // This line is a comment. Append the line to the comment builder.
-            if (currentLine.startsWith(COMMENT_PREFIX)) {
-                // If necessary, append the system dependent line separator at the end of the string.
-                currentComment.append(currentLine.endsWith(lineSeparator) ? currentLine : currentLine + lineSeparator);
-                continue;
-            }
-            // Check of the line is a blank (Doesn't contain any important characters).
-            if (currentLine.isEmpty() || currentLine.equalsIgnoreCase(lineSeparator)) {
-                continue;
-            }
-            // This line is probably a key value pair (arranged as 'key: value')
-            String[] keyValueArray = currentLine.split(KEY_VALUE_SEPARATOR);
-            // If the length is 1, then we'll assume there's no value, therefore assign a default value of null.
-            if (keyValueArray.length == 1) {
-                createKeyValuePair(keyValueArray[0], null);
-            } else {
-                // Assume the length is greater than 1, therefore the key probably has a value
-                // associated with it.
-                createKeyValuePair(keyValueArray[0], keyValueArray[1]);
-            }
-            keyCommentMap.put(keyValueArray[0], currentComment.toString());
-            // Stores the current comment getting built.
-            // Make sure there is nothing in the buffer first by deleting all chars.
-            currentComment.delete(0, currentComment.length());
-        }
-        return keyCommentMap;
-    }
-
-    public void createKeyValuePair(String key, Object value) {
-        if (key == null || key.isEmpty()) {
-            PickpocketPlugin.log("Failed to create key: The key is null.");
+    public void addCommentToKey(String key, String... comment) {
+        if (!configuration.isSet(key)) {
+            System.out.println("No key with name " + key + " found. Skipping comments for this key.");
             return;
         }
-        if (!configuration.contains(key) || !configuration.isSet(key)) {
-            configuration.set(key, value);
-        }
+        commentToKeyMap.put(key, comment);
     }
 
     public void save() {
-        try (BufferedWriter buffWriter = Files.newBufferedWriter(filePath, Files.exists(filePath) ? StandardOpenOption.APPEND : StandardOpenOption.CREATE)) {
+        try (FileWriter fileWriter = new FileWriter(file)) {
             for (String key : configuration.getKeys(true)) {
-                String comment = keyCommentMap.get(key);
-                // Write the comment if one exists.
-                if (comment != null) {
-                    buffWriter.append(comment);
+                Object value = configuration.get(key);
+                String[] commentArray = commentToKeyMap.get(key);
+                if (commentArray != null) {
+                    for (String comment : commentArray) {
+                        fileWriter.append("# ").append(comment).append(System.lineSeparator());
+                    }
                 }
-                // Write the key-value-pair to the file.
-                buffWriter.append(key)
+                fileWriter.append(key)
                         .append(": ")
-                        .append(String.valueOf(configuration.get(key)))
-                        .append(lineSeparator)
-                        .append(lineSeparator);
+                        .append(String.valueOf(value))
+                        .append(System.lineSeparator())
+                        .append(System.lineSeparator());
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -118,14 +68,7 @@ public class CommentedConfig {
         return configuration;
     }
 
-    public void loadConfiguration() {
-        // We'll just use an empty config if there's not an existing file.
-        if (!Files.exists(filePath))
-            return;
-        try {
-            configuration = YamlConfiguration.loadConfiguration(Files.newBufferedReader(filePath));
-        } catch (IOException e) {
-            PickpocketPlugin.log("Error loading configuration: " + filePath + ".");
-        }
+    public void reload() {
+        configuration = YamlConfiguration.loadConfiguration(file);
     }
 }
--- src/logan/config/PickpocketConfiguration.java
+++ src/logan/config/PickpocketConfiguration.java
@@ -2,6 +2,8 @@ package logan.config;
 
 import logan.pickpocket.main.PickpocketPlugin;
 
+import java.io.File;
+import java.util.Collections;
 import java.util.List;
 
 public class PickpocketConfiguration {
@@ -17,7 +19,24 @@ public class PickpocketConfiguration {
     private static CommentedConfig config;
 
     public static void init() {
-        config = new CommentedConfig(PickpocketPlugin.getInstance().getResource("config.yml"), PickpocketPlugin.getInstance().getDataFolder().getPath() + "/config.yml");
+        config = new CommentedConfig(new File(PickpocketPlugin.getInstance().getDataFolder(), "config.yml"));
+
+        config.createKeyIfNoneExists(caughtChanceKey, 0.1);
+        config.createKeyIfNoneExists(minigameRollRateKey, 20);
+        config.createKeyIfNoneExists(cooldownTimeKey, 10);
+        config.createKeyIfNoneExists(pickpocketToggleKey, true);
+        config.createKeyIfNoneExists(statusOnInteractKey, true);
+        config.createKeyIfNoneExists(statusOnLoginKey, true);
+        config.createKeyIfNoneExists(disabledItemsKey, Collections.singletonList("cake"));
+
+        config.addCommentToKey(caughtChanceKey, "The chance a player will get caught while rummaging.");
+        config.addCommentToKey(minigameRollRateKey, "The time in ticks a user has before the", "mini-game inventory slots are randomized again.");
+        config.addCommentToKey(cooldownTimeKey, "The time the player must wait in seconds", "between pick-pocketing attempts.", "An attempt is when a player successfully", "pick-pockets another player.");
+        config.addCommentToKey(pickpocketToggleKey, "Allow players to disable pick-pocketing", "for themselves. This will also disallow others", "from pick-pocketing them.");
+        config.addCommentToKey(statusOnInteractKey, "Whether or not to show a players the", "pick-pocket status message when they attempt", "to pick-pocket another player whilst they, or the", "victim has pick-pocketing disabled.");
+        config.addCommentToKey(statusOnLoginKey, "Whether or not to show a players pick-pocket status when logging in.");
+        config.addCommentToKey(disabledItemsKey, "Items that can't be stolen and therefore, won't show", "up in the rummage GUI. A list of Minecraft IDs can be found", "at www.deadmap.com/idlist");
+
         config.save();
     }
 
@@ -46,6 +65,6 @@ public class PickpocketConfiguration {
     }
 
     public static void reloadConfiguration() {
-        config.loadConfiguration();
+        config.reload();
     }
 }
