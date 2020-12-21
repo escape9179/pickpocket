2372392b507cd4caed116a8ea79b6b66d3dd4c10
Tre Logan <tr3.logan@gmail.com>
2020-12-20 21:18:24 -0500

    Revert 2.5.2 fix

--- src/logan/pickpocket/main/Profiles.java
+++ src/logan/pickpocket/main/Profiles.java
@@ -8,6 +8,15 @@ import org.bukkit.entity.Player;
  */
 public abstract class Profiles {
     public static PickpocketUser get(Player player) {
-        return new PickpocketUser(player);
+        for (PickpocketUser profile : PickpocketPlugin.getProfiles()) {
+            if (profile.getPlayer().getUniqueId().equals(player.getUniqueId())) {
+                profile.setPlayer(player);
+                return profile;
+            }
+        }
+
+        PickpocketUser profile = new PickpocketUser(player);
+        PickpocketPlugin.addProfile(profile);
+        return profile;
     }
 }
