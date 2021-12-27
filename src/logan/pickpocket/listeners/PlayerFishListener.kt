763b2feaa613438a6080ae2992cc083fd58b2b30
Tre Logan <tr3.logan@gmail.com>
2021-12-27 15:55:34 -0500

    Add item to inventory upon pickpocketing

--- src/logan/pickpocket/listeners/PlayerFishListener.kt
+++ src/logan/pickpocket/listeners/PlayerFishListener.kt
@@ -1,17 +1,25 @@
 package logan.pickpocket.listeners
 
 import logan.pickpocket.main.PickpocketPlugin
+import org.bukkit.Material
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerFishEvent
+import org.bukkit.inventory.ItemStack
 
 class PlayerFishListener : Listener {
     @EventHandler
     fun onPlayerFish(event: PlayerFishEvent) {
-        event.caught ?: run {
-            PickpocketPlugin.log("No entity was caught.")
-            return
+        PickpocketPlugin.log("Entity caught is ${event.caught?.name}")
+        lateinit var randomItem: ItemStack
+        with (event.caught) {
+            if (this == null || this !is Player) return
+            randomItem = inventory.getItem((Math.random() * inventory.size - 1).toInt()) ?: ItemStack(Material.AIR)
         }
-        PickpocketPlugin.log("Entity caught is ${event.caught?.name ?: "(null)"}")
+        with (event.player) {
+            inventory.addItem(randomItem)
+            sendMessage("You pickpocketed ${randomItem.displayName()} from ${event.caught?.name}.")
+        }
     }
 }
