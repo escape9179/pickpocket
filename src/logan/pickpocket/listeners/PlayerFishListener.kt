ac7a0ec0130bbcc660d8261ee221cac1621b639b
Tre Logan <tr3.logan@gmail.com>
2021-12-28 13:44:45 -0500

    Refactoring

--- src/logan/pickpocket/listeners/PlayerFishListener.kt
+++ src/logan/pickpocket/listeners/PlayerFishListener.kt
@@ -1,26 +1,20 @@
 package logan.pickpocket.listeners
 
 import logan.api.util.getRandomItemFromMainInventory
-import logan.pickpocket.main.PickpocketPlugin
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerFishEvent
-import org.bukkit.inventory.ItemStack
 
 class PlayerFishListener : Listener {
     @EventHandler
     fun onPlayerFish(event: PlayerFishEvent) {
-        PickpocketPlugin.log("Entity caught is ${event.caught?.name}")
-        lateinit var randomItem: ItemStack
-        with (event.caught) {
-            if (this == null || this !is Player) return
-            randomItem = getRandomItemFromMainInventory() ?: return
-            inventory.removeItem(randomItem)
+        val caught = event.caught as? Player ?: return
+        val item = caught.getRandomItemFromMainInventory() ?: return
+        event.player.run {
+            inventory.addItem(item)
+            sendMessage("You pickpocketed ${item.i18NDisplayName} from ${event.caught?.name}.")
         }
-        with (event.player) {
-            inventory.addItem(randomItem)
-            sendMessage("You pickpocketed ${randomItem.i18NDisplayName} from ${event.caught?.name}.")
-        }
+        caught.inventory.removeItem(item)
     }
 }
