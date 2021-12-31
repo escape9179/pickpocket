69c3adb99051c857ba0d1b64af668cc03fc98ff4
Tre Logan <tr3.logan@gmail.com>
2021-12-31 11:28:35 -0500

    Open thief profile menu

--- src/logan/pickpocket/commands/ProfileCommand.kt
+++ src/logan/pickpocket/commands/ProfileCommand.kt
@@ -2,12 +2,14 @@ package logan.pickpocket.commands
 
 import logan.api.command.BasicCommand
 import logan.api.command.SenderTarget
+import logan.api.gui.InventoryMenu
+import logan.api.gui.PlayerInventoryMenu
 import org.bukkit.entity.Player
 
 class ProfileCommand : BasicCommand<Player>(
     "profile",
     3..3,
-    listOf(String::class),
+    listOf(String::class, String::class, String::class),
     "pickpocket",
     SenderTarget.PLAYER,
     "pickpocket.admin.profile",
@@ -18,6 +20,16 @@ class ProfileCommand : BasicCommand<Player>(
     """.trimIndent()
 ) {
     override fun run(sender: Player, args: Array<out String>, data: Any?): Boolean {
-        TODO("Not yet implemented")
+        sender.openThiefProfileMenu(args[1])
+        return true
     }
+}
+
+fun Player.openThiefProfileMenu(name: String) {
+    val menu = ThiefProfileMenu()
+    menu.show(this)
+}
+
+class ThiefProfileMenu(private val menu: InventoryMenu = PlayerInventoryMenu("Thief Profile", 4)) : InventoryMenu by menu {
+
 }
