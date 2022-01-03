b6ae3b3b32619ad5d32bbc296324548e4f2ae4c6
Tre Logan <tr3.logan@gmail.com>
2022-01-03 02:13:02 -0500

    Implement run for ProfileCommand

--- src/logan/pickpocket/commands/ProfileCommand.kt
+++ src/logan/pickpocket/commands/ProfileCommand.kt
@@ -2,19 +2,27 @@ package logan.pickpocket.commands
 
 import logan.api.command.BasicCommand
 import logan.api.command.SenderTarget
+import logan.pickpocket.user.PickpocketUser
 import org.bukkit.entity.Player
 
 class ProfileCommand : BasicCommand<Player>(
     "profile",
-    3..3,
+    1..3,
     listOf(String::class, String::class, String::class),
     "pickpocket",
     SenderTarget.PLAYER,
     "pickpocket.admin.profile",
     """
         Usage:
+        /pickpocket profile view
         /pickpocket profile create <thief|victim> <name>
         /pickpocket profile remove <thief|victim> <name>
         /pickpocket profile edit <thief|victim> <name> <property> <value>
     """.trimIndent()
-)
+) {
+    override fun run(sender: Player, args: Array<out String>, data: Any?): Boolean {
+        val profile = PickpocketUser.get(sender).findThiefProfile()
+        sender.sendMessage("name: ${profile.name}\n".apply { profile.properties.forEach { (k, v) -> plus("$k:$v\n") } })
+        return true
+    }
+}
